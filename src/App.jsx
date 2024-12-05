db54d40e2f34c9cb667f71e8aa19f47ad5289a59
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-12-05 07:14:30 +0300

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,9 @@ function App() {
             </Routes>
           </main>
           <Footer />
+          <Analytics />
         </div>
       </Router>
-      <Analytics />
     </AuthProvider>
   )
 }
